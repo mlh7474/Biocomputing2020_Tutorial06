1e4e7eccfc5039d95b75514e475ff5a058b6d6a8
Hope Baldwin <hbaldwi2@nd.edu>
2020-09-21 20:48:02 -0400

    Part 2 changes

--- tutorial.sh
+++ tutorial.sh
@@ -2,8 +2,10 @@
 cat $1 | cut -d, -f1,2 | tr "," " " | sort -k1 | sort -k2 > output.txt
 
 echo "Highest: "
-cat $1 | sort -k4n | cut -d, -f1,2,4 | tail -n 1
+cat $1 | cut -d, -f1,2,4 | tr "," " " | sort -k3 -n | tail -n 1
 echo "Lowest: "
-cat $1 | sort -k4n | cut -d, -f1,2,4 | head -n 1 
+cat $1 | cut -d, -f1,2,4 | tr "," " " | sort -k3 -n | head -n 2 | tail -n 1
+
 echo "10 Highest Earners Who are Female"
-cat $1 | sort -k4n | tail -n 10
+cat $1 | cat wages.csv | cut -d, -f1,2,4 | tr "," " " | sort -k3 -n | grep -l "female"  
+
