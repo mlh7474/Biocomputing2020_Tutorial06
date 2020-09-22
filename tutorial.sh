be84cb16e71450afc7468824e5616a2c9737b8b8
Morgan Hiller <mlh7474@yahoo.com>
2020-09-21 20:46:30 -0400

    update

--- tutorial.sh
+++ tutorial.sh
@@ -1,5 +1,5 @@
 #task1
-cat $1 | cut -d, -f1,2 | tr "," " " | sort -k1 | sort -k2 > output.txt
+cat $1 | cut -d, -f1,2 | tr "," " " | sort -k1 | sort -k2 -n > output.txt
 
 echo "Highest: "
 cat $1 | sort -k4n | cut -d, -f1,2,4 | tail -n 1
