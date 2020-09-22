b8699fac3fbbd11e4191f3fc0d5ca7e51129c460
Hope Baldwin <hbaldwi2@nd.edu>
2020-09-21 20:24:37 -0400

    Part 2

--- tutorial.sh
+++ tutorial.sh
@@ -1,2 +1,9 @@
 #task1
 cat $1 | cut -d, -f1,2 | tr "," " " | sort -k1 | sort -k2 > output.txt
+
+echo "Highest: "
+cat $1 | sort -k4n | cut -d, -f1,2,4 | tail -n 1
+echo "Lowest: "
+cat $1 | sort -k4n | cut -d, -f1,2,4 | head -n 1 
+echo "10 Highest Earners Who are Female"
+cat $1 | sort -k4n | tail -n 10
