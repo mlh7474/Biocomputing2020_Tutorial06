e7099dda75ac2a9ebede15cef071737a0927681a
Hope Baldwin <hbaldwi2@nd.edu>
2020-09-21 20:54:55 -0400

    part 2

--- tutorial.sh
+++ tutorial.sh
@@ -7,5 +7,5 @@ echo "Lowest: "
 cat $1 | cut -d, -f1,2,4 | tr "," " " | sort -k3 -n | head -n 2 | tail -n 1
 
 echo "10 Highest Earners Who are Female"
-cat $1 | cat wages.csv | cut -d, -f1,2,4 | tr "," " " | sort -k3 -n | grep -l "female"  
+cat $1 | cut -d, -f1,2,4 | tr "," " " | sort -k3 -n | tail -n 10 | grep -c "female"  
 
